a9c98eb682211432034b496d6e669ff1b67d8ec3
jgardner03 <jgardner03@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-15 04:49:26 +0000

    v0.5
    git-svn-id: https://plugins.svn.wordpress.org/arconix-flexslider/trunk@625520 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -3,188 +3,70 @@
   Plugin Name: Arconix FlexSlider
   Plugin URI: http://www.arconixpc.com/plugins/arconix-flexslider
   Description: A featured slider using WooThemes FlexSlider script.
+
   Author: John Gardner
   Author URI: http://www.arconixpc.com
 
-  Version: 0.2
+  Version: 0.5
 
   License: GNU General Public License v2.0
   License URI: http://www.opensource.org/licenses/gpl-license.php
  */
 
+class Arconix_FlexSlider {
 
-define( 'ACFS_VERSION', '0.2' );
-define( 'ACFS_URL', plugin_dir_url( __FILE__ ) );
-
-add_action( 'after_setup_theme', 'acfs_setup', 15 );
-
-/**
- * Loads required files and functions
- *
- * @since 0.1
- */
-function acfs_setup() {
-
-    add_action( 'wp_enqueue_scripts', 'acfs_scripts' );
-    add_action( 'widgets_init', 'acfs_register_widget' );
-
-}
-
-/**
- * Load required CSS and JavaScript
- *
- * @since 0.1
- */
-function acfs_scripts() {
-
-    wp_register_script( 'flexslider', ACFS_URL . 'js/jquery.flexslider-min.js', array( 'jquery' ), '1.8', true );
-
-    /** Allow user to override css by including his own */
-    if( file_exists( get_stylesheet_directory() . "/arconix-flexslider.css" ) ) {
-	wp_enqueue_style( 'arconix-flexslider', get_stylesheet_directory_uri() . '/arconix-flexslider.css', array( ), ACFS_VERSION );
-    }
-    elseif( file_exists( get_template_directory() . "/arconix-flexslider.css" ) ) {
-	wp_enqueue_style( 'arconix-flexslider', get_template_directory_uri() . '/arconix-flexslider.css', array( ), ACFS_VERSION );
-    }
-    else {
-	wp_enqueue_style( 'arconix-flexslider', ACFS_URL . 'flexslider.css', array( ), ACFS_VERSION );
-    }
-
-    /** Allow user to override javascript by including his own */
-    if( file_exists( get_stylesheet_directory() . "/arconix-flexslider.js" ) ) {
-	wp_register_script( 'arconix-flexslider-js', get_stylesheet_directory_uri() . '/arconix-flexslider.js', array( 'flexslider' ), ACFS_VERSION, true );
-    }
-    elseif( file_exists( get_template_directory() . "/arconix-flexslider.js" ) ) {
-	wp_register_script( 'arconix-flexslider-js', get_template_directory_uri() . '/arconix-flexslider.js', array( 'flexslider' ), ACFS_VERSION, true );
-    }
-    else {
-	wp_register_script( 'arconix-flexslider-js', ACFS_URL . 'js/flexslider.js', array( 'flexslider' ), ACFS_VERSION, true );
-    }
-
-    /** Only load the script if the widget is active */
-    /*if( is_active_widget( '', '', 'arconix-flexslider-widget' ) )*/
-        wp_enqueue_script( 'arconix-flexslider-js' );
-
-}
-
-/**
- * Register the Slider Widget
- *
- * @since 0.1
- */
-function acfs_register_widget() {
-    register_widget( 'Arconix_FlexSlider_Widget' );
-}
-
-require_once( dirname( __FILE__ ) . '/flexslider-widget.php' );
-
-/**
- * Returns registered image sizes.
- *
- * @global array $_wp_additional_image_sizes Additionally registered image sizes
- * @return array Two-dimensional, with width, height and crop sub-keys
- * @since 0.1
- */
-function acfs_get_image_sizes() {
-
-    global $_wp_additional_image_sizes;
-    $additional_sizes = array();
-
-    $builtin_sizes = array(
-	'thumbnail' => array(
-	    'width' => get_option( 'thumbnail_size_w' ),
-	    'height' => get_option( 'thumbnail_size_h' ),
-	    'crop' => get_option( 'thumbnail_crop' ),
-	),
-        'medium' => array(
-	    'width' => get_option( 'medium_size_w' ),
-	    'height' => get_option( 'medium_size_h' ),
-	),
-        'large' => array(
-	    'width' => get_option( 'large_size_w' ),
-	    'height' => get_option( 'large_size_h' ),
-	)
-    );
-
-    if( $_wp_additional_image_sizes )
-	$additional_sizes = $_wp_additional_image_sizes;
-
-    return array_merge( $builtin_sizes, $additional_sizes );
-}
-
-/**
- * Return a modified list of Post Types
- *
- * @return type array Post Types
- * @since 0.1
- * @version 0.2
- */
-function acfs_get_post_types() {
-
-    $post_types = get_post_types( '', 'names' );
-    
-    /** 
-     * List of post types we don't want to show in the select box
-     * This list can be added to by putting a filter in your functions file
+    /**
+     * Boolean for loading the javascript
+     *
+     * @var boolean true|false
+     * @since 0.5
      */
-    $excl_post_types = apply_filters( 'acfs_exclude_post_types',
-        array( 
-            'revision', 
-            'nav_menu_item', 
-            'attachment', 
-            'wpcf7_contact_form' 
-        )
-    );
-    
-    /** Loop through and exclude the items in the list */
-    foreach( $excl_post_types as $excl_post_type ) {
-        if( isset( $post_types[$excl_post_type] ) ) unset( $post_types[$excl_post_type] );
+    public static $load_flex_js;
+
+    /**
+     * Constructor
+     *
+     * @since 0.5
+     */
+    function __construct() {
+        $this->constants();
+        $this->hooks();
     }
 
-    return $post_types;
+    /**
+     * Define the constants
+     *
+     * @since 0.5
+     */
+    function constants() {
+        define( 'ACFS_VERSION', '0.5');
+        define( 'ACFS_URL', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+        define( 'ACFS_INCLUDES_URL', trailingslashit( ACFS_URL . 'includes' ) );
+        define( 'ACFS_IMAGES_URL', trailingslashit( ACFS_URL . 'images' ) );
+        define( 'ACFS_JS_URL', trailingslashit( ACFS_INCLUDES_URL . 'js' ) );
+        define( 'ACFS_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+        define( 'ACFS_INCLUDES_DIR', trailingslashit( ACFS_DIR . 'includes' ) );
+    }
+
+    /**
+     * Run the necessary functions and pull in the necessary supporting files
+     *
+     * @since 0.5
+     */
+    function hooks() {
+        add_action( 'wp_enqueue_scripts', 'load_scripts' );
+        add_action( 'init', 'register_shortcodes' );
+        add_action( 'widgets_init', 'create_widget' );
+        add_action( 'wp_dashboard_setup', 'register_dashboard_widget' );
+        add_action( 'wp_footer', 'print_scripts' );
+
+        require_once( ACFS_INCLUDES_DIR . 'functions.php' );
+        require_once( ACFS_INCLUDES_DIR . 'widget.php' );
+        if( is_admin() )
+            require_once( ACFS_INCLUDES_DIR . 'admin.php' );
+    }
+
 }
 
-/**
- * Register the dashboard widget
- * 
- * @since 0.1 
- */
-function acfs_register_dashboard_widget() {
-    wp_add_dashboard_widget( 'ac-flexslider', 'Arconix FlexSlider', array( $this, 'dashboard_widget_output' ) );
-}
-
-/**
- * Output for the dashboard widget
- *
- * @since 0.1
- */
-function acfs_dashboard_widget_output() {
-    echo '<div class="rss-widget">';
-
-    wp_widget_rss_output( array(
-        'url' => 'http://arconixpc.com/tag/arconix-flexslider/feed', // feed url
-        'title' => 'Arconix FlexSlider Posts', // feed title
-        'items' => 3, // how many posts to show
-        'show_summary' => 1, // display excerpt
-        'show_author' => 0, // display author
-        'show_date' => 1 // display post date
-    ) );
-
-    echo '<div class="acfs-widget-bottom"><ul>'; ?>
-                <li><img src="<?php echo ACFS_URL . 'images/page-16x16.png'; ?>"><a href="http://arcnx.co/afswiki">Wiki Page</a></li>
-                <li><img src="<?php echo ACFS_URL . 'images/help-16x16.png'; ?>"><a href="http://arcnx.co/afshelp">Support Forum</a></li>
-    <?php
-    echo '</ul></div>';
-    echo '</div>';
-
-    // handle the styling
-    echo '<style type="text/css">
-            #ac-flexslider .rsssummary { display: block; }
-            #ac-flexslider .acfs-widget-bottom { border-top: 1px solid #ddd; padding-top: 10px; text-align: center; }
-            #ac-flexslider .acfs-widget-bottom ul { list-style: none; }
-            #ac-flexslider .acfs-widget-bottom ul li { display: inline; padding-right: 9%; }
-            #ac-flexslider .acfs-widget-bottom img { padding-right: 3px; vertical-align: middle; }
-        </style>';
-}
-
+new Arconix_FlexSlider;
 ?>
--- readme.txt
+++ readme.txt
@@ -1,15 +1,16 @@
-=== Plugin Name ===
+=== Arconix Flexslider ===
 Contributors: jgardner03
+Donate link: http://arcnx.co/acfsdonation
 Tags: arconix, flexslider, slider
-Requires at least: 3.1
-Tested up to: 3.3.1
-Stable tag: 0.2.1
+Requires at least: 3.4
+Tested up to: 3.4.2
+Stable tag: 0.5
 
 A multi-purpose responsive jQuery slider that supports custom post types and responsive themes.
 
 == Description ==
 
-Arconix Flexslider is a plugin that utilizes the excellent, responsive FlexSlider jQuery plugin. This plugin supports a user-selected post types in addition to posts and pages on a per-widget basis, which is perfect for featured posts or recent portfolio items on the homepage
+Arconix Flexslider is a plugin that utilizes the excellent, responsive FlexSlider jQuery plugin. This plugin supports a user-selected post types in addition to posts and pages on a per-widget basis, which is perfect for featured posts or recent portfolio items on the homepage. It can be called by placing the widget in a widget area or by using the shortcode `[ac-flexslider]`
 
 = Features =
 * Resizes based on the user's device
@@ -23,28 +24,36 @@ Activate Arconix-FlexSlider in the "Plugins" admin panel using the "Activate" li
 
 == Upgrade Notice ==
 
-Upgrade normally via your Wordpress admin -> Plugins panel.
+Upgrade normally via your WordPress admin -> Plugins panel.
 
 == Frequently Asked Questions ==
 
-= Where can I find more information on how to use the plugin? =
+= How do I use the slider?  =
 
-* Visit the plugin's [Wiki Page](http://arcnx.co/afswiki "Arconix FlexSlider Wiki") for documentation on the widget
+* Place the Arconix - Flexslider widget in the desired widget area
+* Use the shortcode `[ac-flexslider]` on a post, page or other area
+* Place `<?php echo do_shortcode( "[ac-flexslider]" ); ?>` in the desired page template
+
+= Is there any other documentation? =
+* Visit the plugin's [Wiki Page](http://arcnx.co/afswiki) for all the plugin's documentation
 * Tutorials on advanced plugin usage can be found at [Arconix Computers](http://arconixpc.com/tag/arconix-flexslider)
 
+= The slider isn't styled properly =
+* With no 2 themes exactly alike, it's extremely difficult to style a plugin that seamlessly integrates without issue. That's why I made the plugin flexible -- If you'd like tighter integration between your theme and the flexslider, copy `includes/flexslider.css` to the root of your theme's folder and rename it to `arconix-flexslider.css`. My plugin will try to load that file first, which means you can make your changes and not risk losing them on a plugin upgrade.
+
 = What is responsive design? =
-Responsive design is in essence designing a website to cater to the dimensions of the user's device. It has become far more popular with the proliferation of web-enabled smartphones and tablets.
+Responsive design is in essence designing a website to cater to the dimensions of the user's device. It has become very popular with the proliferation of web-enabled smartphones and tablets.
 
 = Do I need a responsive theme to use this plugin? =
 Absolutely not. The slider will conform to the dimensions provided to it
 
-= I have a problem or I found a bug =
+= I need help, or I found a bug =
 
-Check out the WordPress [support forum](http://arcnx.co/afshelp)
+* Check out the WordPress [support forum](http://arcnx.co/afshelp) or the [Issues section on Github](http://arcnx.co/afsissues)
 
-= Can you add this feature or that option? =
+= I have a great idea for your plugin! =
 
-I can certainly look into it. Contact me through [Twitter](http://arcnx.co/twitter), [Facebook](http://arcnx.co/facebook) or my [Website](http://arcnx.co/1 "Arconix Computers").
+That's fantastic! Feel free to submit a pull request over at [Github](http://arcnx.co/afssource), or you can contact me through [Twitter](http://arcnx.co/twitter), [Facebook](http://arcnx.co/facebook) or my [Website](http://arcnx.co/1).
 
 == Screenshots ==
 1. Widget Options overview
@@ -53,12 +62,18 @@ I can certainly look into it. Contact me through [Twitter](http://arcnx.co/twitt
 
 == Changelog ==
 
-= 0.2.1 - 2012.05.03 =
+= 0.5 =
+* Added ability to call the slider from a shortcode
+* Added the ability to display the chosen post-type's content (either in whole or just the excerpt)
+* Added the option to display just the images without link tags (making the images un-clickable)
+* Added the ability to display specific categories or tags (works with 'posts' only)
+
+= 0.2.1 =
 * fixed ie8 image scale issue
 
-= 0.2 - 2012.04.16 =
+= 0.2 =
 * Added additional options for image captions
 * Only return posts to display if they have featured images
 
-= 0.1 - 2012.03.23 =
+= 0.1 =
 * Initial release
